2bd337610777c4b42cf1284f33d18b07219a6f88
shubham.inexture <shubhamsinha.inexture@gmail.com>
2021-06-23 20:29:55 +0530

    Initial Commit

--- socialmediaintegration/settings.py
+++ socialmediaintegration/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4^ftm+(19lv6$$chz(43rbs3!=g6)j(@-la!!=l9%2b7xv(slp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
